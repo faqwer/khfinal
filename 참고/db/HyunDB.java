7e34af54ae46f25cdef28ccba652d06c2f613081
Hyun <guswo9024@gmail.com>
2017-12-02 19:03:06 +0900

    참고자료 추가

---
+++ 참고/db/HyunDB.java
@@ -0,0 +1,24 @@
+package hyun.db;
+
+import java.sql.Connection;
+
+import javax.naming.*;
+import javax.sql.*;
+
+public class HyunDB {
+	static DataSource ds;
+	static{
+		try {
+			Context initContext = new InitialContext();
+			Context envContext  = (Context)initContext.lookup("java:/comp/env");
+			ds = (DataSource)envContext.lookup("jdbc/myoracle");
+		}catch(NamingException e){
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+		}
+	}
+	
+	public static Connection getConn() throws Exception{
+		return ds.getConnection();
+	}
+}
---
+++ 참고/paging/PageModule.java
@@ -0,0 +1,54 @@
+package hyun.paging;
+
+public class PageModule {
+
+	// 프로그램이 시작하는 시점에서 메모리에 올라가기 때문에 객체를 만들지 않고도 사용이 가능! = static
+	public static String makePage(String pagename,int totalCnt,int listSize,int pageSize,int cp) { 
+	      
+	      StringBuffer sb=new StringBuffer();
+
+	      int totalPage=totalCnt/listSize+1;
+	      if(totalCnt%listSize==0) {
+	    	  if(totalCnt!=0) {
+	    	  totalPage--;
+	    	  }
+	      }
+	      int userGroup=cp/pageSize;
+	      if(cp%pageSize==0)userGroup--;
+	      
+	      //왼쪽으로 가는 화살표
+	      if(userGroup!=0){
+	         sb.append("<a href='");
+	         sb.append(pagename);
+	         sb.append("?cp=");
+	         int temp=(userGroup-1)*pageSize+pageSize;
+	         sb.append(temp);
+	         sb.append("'>&lt;&lt;</a>");
+	      }
+	      //숫자 출력
+	      for(int i=(userGroup*pageSize+1); i<=(userGroup*pageSize+pageSize); i++){
+	         sb.append("&nbsp;&nbsp;<a href='");
+	         sb.append(pagename);
+	         sb.append("?cp=");
+	         sb.append(i);
+	         sb.append("'>");
+	         sb.append(i);
+	         sb.append("</a>&nbsp;&nbsp;");
+	         if(i==totalPage)break;
+	      }
+	      
+	      //오른쪽으로 가는 화살표
+	      
+	      if(userGroup!=(totalPage/pageSize-(totalPage%pageSize==0?1:0))){
+	         sb.append("<a href='");
+	         sb.append(pagename);
+	         sb.append("?cp=");
+	         int temp=(userGroup+1)*pageSize+1;
+	         sb.append(temp);
+	         sb.append("'>&gt;&gt;</a>");
+	      }
+	      
+	      return sb.toString();
+	      
+	   }
+}
