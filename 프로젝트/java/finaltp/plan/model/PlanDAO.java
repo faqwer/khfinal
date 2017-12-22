1ee3f235885a8197d429e1e1ca21dd0e4fae89b7
Hyun <guswo9024@gmail.com>
2017-12-22 16:52:36 +0900

    planDao update

--- 프로젝트/java/finaltp/plan/model/PlanDAO.java
+++ 프로젝트/java/finaltp/plan/model/PlanDAO.java
@@ -1,7 +1,15 @@
 package finaltp.plan.model;
 
-import java.util.List;
-
 public interface PlanDAO {
-	public List<PlanDTO> planList(int cp, int ls, String status); // í”Œëž˜ë„ˆ ë¦¬ìŠ¤íŠ¸
+	public int planWrite(PlanDTO pdto);
+	public int planUpdate(PlanDTO pdto);
+	public int planDelete(int planner_idx);
+	public PlanDTO getPlanContent(int planner_idx);
+	public int getPlanRecommendCnt(int planner_idx);
+	public int ckRecommend(int planner_idx,int user_idx);
+	public int addRecommend(int planner_idx,int writer_idx,int user_idx);
+	public int delRecommend(int planner_idx,int writer_idx,int user_idx);
+	public String getUesrid(int member_idx);
+	public List<PlanDTO> planList(int cp, int ls, String status); // ÇÃ·¡³Ê ¸®½ºÆ®
+	
 }
